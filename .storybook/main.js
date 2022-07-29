8f898bc5bdc351eb80254902864046152830f088
indralukmana <indralukmana@live.com>
2022-07-29 12:19:43 +0700

    feat: cleanup Storybook stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     'storybook-addon-next',
+    'storybook-addon-swc',
   ],
   framework: '@storybook/react',
   core: {
