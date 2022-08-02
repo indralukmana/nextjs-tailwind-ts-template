bc8e8ba622cd0879bc02f216b856610215ae2443
indralukmana <indralukmana@live.com>
2022-08-02 12:42:09 +0700

    feat: add getLayout configurations for page

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,6 +1,21 @@
+import Head from 'next/head';
 import * as React from 'react';
 
-export default function AppLayout({ children }: { children: React.ReactNode }) {
+export type AppLayoutProps = { children: React.ReactNode; pageTitle?: string };
+
+const AppLayout: React.FC<AppLayoutProps> = ({
+  children,
+  pageTitle = '~Page Title~',
+}) => {
   // Put Header or Footer Here
-  return <>{children}</>;
-}
+  return (
+    <div className="min-h-full bg-blue-400">
+      <Head>
+        <title>{pageTitle}</title>
+      </Head>
+      {children}
+    </div>
+  );
+};
+
+export default AppLayout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,17 @@ import type { AppProps } from 'next/app';
 
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { NextPageWithLayout } from '@/pages/page';
+
+interface AppPropsWithLayout extends AppProps {
+  Component: NextPageWithLayout;
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return getLayout(<Component {...pageProps} />);
 }
 
 export default MyApp;
