aa1b23ff54ae2fd603c547392f6ef3a81f7ed3ff
indralukmana <indralukmana@live.com>
2022-09-05 08:41:46 +0700

    feat: add docker configs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
 
